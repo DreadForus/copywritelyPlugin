3239c93ac3e81035bc46205a328470222d28a0bd
petrossi <edipetrossi@gmail.com>
2018-10-10 11:36:22 +0300

    push

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.browserAction.onClicked.addListener(function(activeTab){
         var tab = tabs[0];
         var url = tab.url;
 
-        const url = "https://dev.copywritely.com/tools/copywritely/create_new_task_from_plugin?url=" + url;
+        const url = "https://copywritely.com/tools/copywritely/create_new_task_from_plugin?url=" + url;
 
         chrome.tabs.create({ url: url });
     });
